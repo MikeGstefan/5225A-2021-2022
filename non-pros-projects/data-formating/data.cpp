d372e6af8b5c259d8272a6a92830932f4c302de2
Mike Stefan <mikeGstefan66@gmail.com>
2021-05-23 16:53:01 -0400

    data for vs code without sd card

---
+++ non-pros-projects/data-formating/data.cpp
@@ -0,0 +1,30 @@
+#include "data.hpp"
+
+Data tracking(Levels::wire);
+
+void Data::print(const char* format, ...){
+    char output[256];
+    va_list args;
+    va_start(args, format);
+    vsprintf(output, format, args);
+    switch(this->level){
+        case Levels::both:
+            printf("%s\n",output);
+        break;
+        case Levels::wire:
+            printf("%s\n",output);
+        break;
+        case Levels::card:
+
+        break;
+        case Levels::none:
+
+        break;
+    }
+}
+
+int main()
+{   
+    int pain = 36;
+    tracking.print("testing %d",pain);
+}
