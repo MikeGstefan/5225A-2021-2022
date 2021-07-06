1d05eb2175340c84db0756cce02d9848cabf3c65
Mike Stefan <mikeGstefan66@gmail.com>
2021-07-06 13:53:46 -0400

    initial drive code written

---
+++ main/src/config.cpp
@@ -0,0 +1,4 @@
+#include "config.hpp"
+
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor front_l(1,false), front_r(2,false), back_l(3,false),back_r(4,false);
---
+++ main/src/util.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "main.h"
+#include "config.hpp"
+
+void move(int x, int y, int a);
+void move(double x, double y, double a);
