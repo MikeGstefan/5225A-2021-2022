8f2f5ed6cc7af4a31478441e1e9db9184a4265bf
Mike Stefan <mikeGstefan66@gmail.com>
2021-11-02 16:54:30 -0400

    FINAL fixed logging prints & removed tests

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -68,64 +68,9 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 
-int test = 0;
-
-
-void task_testing(void* params){
-	Tasks* task_ptr = Tasks::get_obj(params);
-	while(true){
-		printf("ur mom\n");
-		if(task_ptr->notify_handle())return;
-		printf("data: %d\n",test);
-		delay(1000);
-	}
-}
-
-void task_testing2(void* params){
-	Tasks* task_ptr = Tasks::get_obj(params);
-	while(true){
-		printf("in rebound\n");
-		if(task_ptr->notify_handle())return;
-		delay(1000);
-	}
-}
-
-
-
-
 
 void opcontrol() {
-	printf("here1\n");
-	Tasks test_task(task_testing);
-	printf("here2\n");
-	test_task.start();
-	printf("here4\n");
-	delay(300);
-	test_task.rebind(task_testing2);
-	delay(3000);
-	test_task.kill();
 
-	// test_task.data_update();
-	delay(100);
-	while(true){
-		// std::cout<< test_task.get_task_ptr()->get_state()<<"\n";
-		printf("in loop \n");
-		delay(100);
-	}
 
-	// test = 1;
-	// test_task.done_update();
 
-	// tester = new Task(task_testing);
-	// int timer  = micros();
-	// while(tester->get_state() ==1)delay(10);
-	// std::cout<<micros()-timer<<" "<< tester->get_state()<<"\n";
-	// tester->notify();
-	// tester->notify();
-	// tester->notify();
-	// std::cout<< tester->get_state()<<"\n";
-	while(true){
-		// std::cout<<"not in ur mom"<< tester->get_state()<<"\n";
-		delay(50);
-	}
 }
