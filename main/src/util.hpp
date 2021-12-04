1ca8add6bae250e59ce48f160004c21965897c9f
Nikhil Gante <odyseus05@gmail.com>
2021-12-04 16:55:22 -0500

    Added constrain function to util and commented other util functions

--- main/src/util.hpp
+++ main/src/util.hpp
@@ -29,24 +29,39 @@ double deg_to_rad(double deg);
 
 double near_angle(double angle, double reference);
 
-template <typename T> int sgn(T val) { return (T(0) < val) - (val < T(0)); }
+// restricts a value to a range
+template <typename T> T constrain(T value, T min, T max){
+  return value > min ? value < max ? value : max : min;
+}
 
-template <typename T> T map(T x, T in_min, T in_max, T out_min, T out_max) { return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; }
+// gets the sign of a value (0, 1 or -1)
+template <typename T> int sgn(T value){
+  return (T(0) < value) - (value < T(0));
+}
 
+// maps a value to a range
+template <typename T> T map(T x, T in_min, T in_max, T out_min, T out_max){
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+// base case for recrusive function map_set
 template <typename T>
 T map_set(T input, T in_min, T in_max, T out_min, T out_max, T range, T val) {
   if (input <= range) return map(input, in_min, range, out_min, val);
 }
-
+// maps a values to a set of maps (a piecewise function)
 template <typename T, typename... Ts>
 T map_set(T input, T in_min, T in_max, T out_min, T out_max, T range1, T val_1, Ts... args) {
   if (input <= range1) return map(input, in_min, range1, out_min, val_1);
   else return map_set(input, range1, in_max, val_1, out_max, args...);
 }
 
-template <typename T> T operator++ (T enum_type, int) { return static_cast<T>(static_cast<int>(enum_type) + 1); }
-template <typename T> T operator-- (T enum_type, int) { return static_cast<T>(static_cast<int>(enum_type) - 1); }
+template <typename T> T operator++ (T enum_type, int) {
+  return static_cast<T>(static_cast<int>(enum_type) + 1);
+}
 
+template <typename T> T operator-- (T enum_type, int) {
+  return static_cast<T>(static_cast<int>(enum_type) - 1);
+}
 
 template <typename T, typename = typename std::enable_if<std::is_enum<T>::value, void>::type> T next_enum_value(T enum_type) {
   if (static_cast<int>(enum_type) < static_cast<int>(T::NUM_OF_ELEMENTS) - 1) return enum_type++;
