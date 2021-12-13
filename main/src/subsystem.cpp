4ea6845db45bed6858b0a3247663250a85cfd2f2
Nikhil Gante <odyseus05@gmail.com>
2021-12-12 22:17:22 -0500

    Worked subsystem template class, won't build yet

--- main/src/subsystem.cpp
+++ main/src/subsystem.cpp
@@ -1,26 +1,80 @@
 #include "subsystem.hpp"
 
-Subsystem::Subsystem(Motor& motor, const char* name): Motor(motor), name(name){}
+// Subsystem::Subsystem(Motor& motor, const char* name, const char* state_names[]): Motor(motor), name(name), state_names(state_names){}
+//
+// void Subsystem::handle(){}
+//
+// void Subsystem::set_state(const states next_state){
+//   printf("Lift | Going from %s to %s\n", state_names[state], state_names[next_state]);
+//   last_state = state;
+//   state = next_state;
+// }
+//
+//
+// void Subsystem::reset(){
+//   move(-60);
+//   Timer vel_rise_timeout("vel_rise");
+//   // waits for motor's velocity to rise or timeout to trigger
+//   while(fabs(get_actual_velocity()) < 45.0){
+//     printf("%s's velocity is (rising loop): %lf\n", name, get_actual_velocity());
+//     if (vel_rise_timeout.get_time() > 50){
+//       printf("%s's rising loop timed out\n", name);
+//       break;
+//     }
+//     delay(10);
+//   }
+//   printf("%s's velocity done rising\n", name);
+//   // waits until motors velocity slows down for 5 cycles
+//   cycleCheck(fabs(get_actual_velocity()) < 5.0, 5, 10)
+//   tare_position();  // resets subsystems position
+//   printf("%d, %s's reset %lf\n", millis(), name, get_position());
+//   move(0);
+//
+// }
 
-void Subsystem::handle(){}
 
-void Subsystem::reset(){
-  move(-60);
-  Timer vel_rise_timeout("vel_rise");
-  // waits for motor's velocity to rise or timeout to trigger
-  while(fabs(get_actual_velocity()) < 45.0){
-    printf("%s's velocity is (rising loop): %lf\n", name, get_actual_velocity());
-    if (vel_rise_timeout.get_time() > 50){
-      printf("%s's rising loop timed out\n", name);
-      break;
-    }
-    delay(10);
+// example of Subsystem object
+  /*
+  Subsystem<six_bar_states, true, 6> example(f_bar, "Six_bar", {
+  "searching",
+  "lowered",
+  "raised",
+  "platform",
+  "released",
+  "lowering"}
+  );
+  */
+
+// Example of Six_bar object(child of subsystem class)
+
+  // constructor
+  Six_bar::Six_bar(Subsystem<six_bar_states, true, 6> subsystem): Subsystem(subsystem)
+  {}
+
+  // example of polymorphism
+  void Six_bar::handle(){
+
+
   }
-  printf("%s's velocity done rising\n", name);
-  // waits until motors velocity slows down for 5 cycles
-  cycleCheck(fabs(get_actual_velocity()) < 5.0, 5, 10)
-  tare_position();  // resets subsystems position
-  printf("%d, %s's reset %lf\n", millis(), name, get_position());
-  move(0);
+  // actual object
+  /*
+  Six_bar six_bar = Six_bar(Subsystem<six_bar_states, true, 6>(f_bar, "six_bar",
+  std::array<const char*, 6>{
+  "searching",
+  "lowered",
+  "raised",
+  "platform",
+  "released",
+  "lowering"}
+  ));
+  */
 
-}
+
+  Six_bar six_bar(Subsystem<six_bar_states, true, 6>(f_bar, "Six_bar", {  // this line is causing the error
+  "searching",
+  "lowered",
+  "raised",
+  "platform",
+  "released",
+  "lowering"}
+  ));
--- main/src/subsystem.hpp
+++ main/src/subsystem.hpp
@@ -1,12 +1,75 @@
+#pragma once
 #include "util.hpp"
 #include "timer.hpp"
+#include "config.hpp"
 
 using namespace pros;
 
+template <typename state_type, bool motorized, int num_of_states>
 class Subsystem: public Motor {
+  state_type state;
+  state_type last_state;
+
 public:
   const char* name;
-  Subsystem(Motor& motor, const char* name);  // constructor
-  void handle();  // has a switch containg the state machine for a given subsystem
-  void reset();
+  std::array<const char*, num_of_states> state_names;
+
+
+  // constructor for motorized subsystem
+  template <class = std::enable_if<motorized>>
+  Subsystem(Motor& motor, const char* name, std::array<const char*, num_of_states> state_names);
+
+
+  // constructor for non-motorized subsystem
+  template <class = std::enable_if<!motorized>>
+  Subsystem(const char* name, std::array<const char*, num_of_states> state_names);
+
+
+  void set_state(const state_type next_state){
+    printf("%s | Going from %s to %s\n", state_names[state], state_names[next_state]);
+    last_state = state;
+    state = next_state;
+  }
+
+  void handle();  // has a switch containing the state machine for a given subsystem
+
+  // only compiles the reset method for motorized subsystems
+  template <class = std::enable_if<motorized>>
+  void reset(){
+    move(-60);
+    Timer vel_rise_timeout("vel_rise");
+    // waits for motor's velocity to rise or timeout to trigger
+    while(fabs(get_actual_velocity()) < 45.0){
+      printf("%s's velocity is (rising loop): %lf\n", name, get_actual_velocity());
+      if (vel_rise_timeout.get_time() > 50){
+        printf("%s's rising loop timed out\n", name);
+        break;
+      }
+      delay(10);
+    }
+    printf("%s's velocity done rising\n", name);
+    // waits until motors velocity slows down for 5 cycles
+    cycleCheck(fabs(get_actual_velocity()) < 5.0, 5, 10)
+    tare_position();  // resets subsystems position
+    printf("%d, %s's reset %lf\n", millis(), name, get_position());
+    move(0);
+  }
+};
+
+
+enum class six_bar_states{
+  searching,  // at lowest height waiting for lim switch to pickup goals
+  lowered,  //  at lowest height, not waiting to pikcup mogos
+  raised, // at height for intake to fill alliance mogo
+  platform, // holding mogo at platform dropoff height
+  released, // mogo is released at platform height
+  lowering  // on the way to lowered state from released
+};
+
+class Six_bar: public Subsystem<six_bar_states, true, 6> {
+
+public:
+  Six_bar(Subsystem<six_bar_states, true, 6> subsystem);  // constructor
+  void handle();  // contains state machine code
+
 };
