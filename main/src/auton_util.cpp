7f95536ebe80e9aa18c66d41e5ff7db6a7cbe663
Nikhil Gante <odyseus05@gmail.com>
2022-01-04 12:49:07 -0500

    oops fixed some of the math

--- main/src/auton_util.cpp
+++ main/src/auton_util.cpp
@@ -25,49 +25,54 @@ double get_filtered_output(ADIUltrasonic sensor, int check_count, uint16_t lower
 
 void flatten_against_wall(bool right){
   bool front_done, back_done;
-  int flatten_power = 50, power_diff = 15;
+  int front_count = 0, back_count = 0;
   double front_velocity, back_velocity;
   const double dist_b = 3.0;  // make sure to keep this same as in tracking
   const double simulated_radius = 8.0;
   const double radial_scalar = simulated_radius / dist_b; // how much to multiples the actual radius by to get the simulated radius
-  double angular_component;
+  double angular_component, linear_component;
+  const int cycle_threshold = 20;
+  const double velocity_threshold = 0.5;  // in inches/sec
+  int flatten_power, power_diff;
 
+  if(right) flatten_power = 60, power_diff = 20;
+  else  flatten_power = -60, power_diff = -20;
 
-  if(right){
-    const double velocity_threshold = 0.5;  // in inches/sec
-    drivebase.move(flatten_power, 0.0, 0.0);
-    delay(100); // waits for robots velocity to rise
+  delay(100); // waits for robots velocity to rise
+  drivebase.move(flatten_power, 0.0, 0.0);
 
-    while(!(front_done && back_done)){
-      angular_component = tracking.g_velocity.angle * dist_b;
-      back_velocity = tracking.b_velo * angular_component * radial_scalar;
-      front_velocity = back_velocity - 2 * simulated_radius * tracking.g_velocity.angle;
+  // while(!(front_done && back_done)){
+  while(true){
+    angular_component = tracking.g_velocity.angle * dist_b * radial_scalar;
+    linear_component = tracking.b_velo - angular_component;
 
-      front_done = front_velocity < velocity_threshold;
-      back_done = back_velocity < velocity_threshold;
+    back_velocity = linear_component + angular_component;
+    front_velocity = linear_component - angular_component;
 
-      if(front_done) drivebase.move(30.0, 0.0, -power_diff);
-      else if(back_done) drivebase.move(30.0, 0.0, power_diff);
-      else drivebase.move(flatten_power, 0.0, 0.0);
+    printf("Velocities | back: %lf, front: %lf\n", back_velocity, front_velocity);
 
+    if (fabs(front_velocity) < velocity_threshold){
+      front_done = front_count > cycle_threshold;
+      if(!front_done) front_count++;
     }
-  }
-  else{
-    const double velocity_threshold = -0.5;  // in inches/sec
-    drivebase.move(-flatten_power, 0.0, 0.0);
-    delay(100); // waits for robots velocity to rise
-
-
-    while(!(front_done && back_done)){
-      back_velocity = tracking.b_velo * radial_scalar;
-      front_velocity = back_velocity - 2 * simulated_radius * tracking.g_velocity.angle;
-
-      front_done = front_velocity > velocity_threshold;
-      back_done = tracking.b_velo > velocity_threshold;
-
-      if(front_done) drivebase.move(30.0, 0.0, power_diff);
-      else if(back_done) drivebase.move(30.0, 0.0, -power_diff);
-      else drivebase.move(flatten_power, 0.0, 0.0);
+    else{ // resets front counter if robot is moving
+      front_done = false;
+      front_count = 0;
     }
+
+    if (fabs(back_velocity) < velocity_threshold){
+      back_done = back_count > cycle_threshold;
+      if(!back_done) back_count++;
+    }
+    else{ // resets back counter if robot is moving
+      back_count = 0;
+      back_done = false;
+    }
+
+    if(front_done) drivebase.move(flatten_power, 0.0, -power_diff); // turn back end to align with wall if front is aligned
+    else if(back_done) drivebase.move(flatten_power, 0.0, power_diff); //  turn front end to align with wall if back is aligned
+    else drivebase.move(flatten_power, 0.0, 0.0); // otherwise continune towards wall
+
+    delay(10);
   }
 }
